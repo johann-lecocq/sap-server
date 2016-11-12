2b888cc99a135ec42510c4ef76f0f75f52a27c41
Johann Lecocq <git-jo@johann-lecocq.fr>
2016-11-12 14:19:59 +0100

    test with gunicorn and up version

--- run.py
+++ run.py
@@ -1,24 +1,27 @@
+
 __author__ = "Johann Lecocq(johann-lecocq.fr)"
 __license__ = "GNU GENERAL PUBLIC LICENSE version 2"
-__version__ = "1.1"
+__version__ = "1.2"
 
-import cherrypy
-from paste.translogger import TransLogger
 from flask import Flask
-
 from application import routes
 
+app = Flask("sap")
+app.register_blueprint(routes)
+
 if __name__ == "__main__":
-    app = Flask("sap")
-    app.register_blueprint(routes)
+    
+    import cherrypy
+    from paste.translogger import TransLogger
+    
     app_logged = TransLogger(app)
     cherrypy.tree.graft(app_logged, '/')
     cherrypy.config.update({
-        'engine.autoreload.on': False,
+        'engine.autoreload.on': True,
         'log.screen': True,
         'server.socket_port': 8007,
         'server.socket_host': '127.0.0.1'
     })
+    
     cherrypy.engine.start()
     cherrypy.engine.block()
-
